c18eb9f341c21c9f7bd9643ac88ee2fb124a176c
MacherelR <64424331+MacherelR@users.noreply.github.com>
2025-07-14 14:00:35 +0200

    Fixed squid image with ssl settings

--- squid/entrypoint.sh
+++ squid/entrypoint.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 set -e
 
+# Initialize SSL certificate DB if not present
+SQUID_DIR="/var/lib/squid"
+SSL_DB_DIR="/var/lib/squid/ssl_db"
+
+if [ ! -d "$SQUID_DIR" ]; then
+  echo "Creating ssl_db directory..."
+  mkdir -p "$SQUID_DIR"
+  chown -R proxy:proxy "$SQUID_DIR"
+  chmod -R 700 "$SQUID_DIR"
+
+  echo "Initializing ssl_db at $SSL_DB_DIR..."
+  su -s /bin/sh proxy -c "/usr/lib/squid/security_file_certgen -c -s $SSL_DB_DIR -M 4MB"
+fi
+
 rm -fr /var/log/squid/*
 mkfifo /var/log/squid/access.log
 mkfifo /var/log/squid/cache.log
